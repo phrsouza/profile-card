dab2d44980d21ba40ed2106b28fafec760e737b7
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-17 17:27:45 -0300

    add avatar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
+function Avatar() {
+  return <img className="avatar" src="avatar.jpeg" alt="Pedro Souza" />;
+}
+
 function App() {
-  return <div className="App">Hello Profile Card</div>;
+  return (
+    <div className="card">
+      Hello Profile Card
+      <Avatar />
+    </div>
+  );
 }
 
 export default App;
